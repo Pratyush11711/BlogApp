dfb217de2d2df0b03b9306bc0a0e8e4888339d9e
Pratyush Gupta <pratyush.epi.spn117@gmail.com>
2024-09-22 17:06:27 +0530

    pta nhi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,29 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const path=request.nextUrl.pathname
-  const isPublicPath=path==='/login' || path==='/register'
-  const token=request.cookies.get("token")?.value ||''
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/', request.url))
-  }
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-}
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request: NextRequest) {
+//   const path=request.nextUrl.pathname
+//   const isPublicPath=path==='/login' || path==='/register'
+//   const token=request.cookies.get("token")?.value ||''
+//   if(isPublicPath && token){
+//     return NextResponse.redirect(new URL('/', request.url))
+//   }
+//   if(!isPublicPath && !token){
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
+// }
  
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: [
-    '/register',
-    '/login',
-    '/blog',
-    '/about',
-    '/contact',
-    '/createBlog',
-    '/'
-  ],
-}
+// export const config = {
+//   matcher: [
+//     '/register',
+//     '/login',
+//     '/blog',
+//     '/about',
+//     '/contact',
+//     '/createBlog',
+//     '/'
+//   ],
+// }
+
