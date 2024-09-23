cec0a13a730e904061d31c4c08a2a3fb504bcd34
Pratyush Gupta <pratyush.epi.spn117@gmail.com>
2024-09-23 23:55:33 +0530

    ffd

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,43 +1,20 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const path=request.nextUrl.pathname
-  const isPublicPath=path==='/login' || path==='/register'
-  const token=request.cookies.get("token")?.value ||''
-  if(isPublicPath && token){
-    return NextResponse.redirect(new URL('/', request.url))
-  }
-  if(!isPublicPath && !token){
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: [
-    '/register',
-    '/login',
-    '/blog',
-    '/about',
-    '/contact',
-    '/createBlog',
-    '/'
-  ],
-}
-
 // import { NextResponse } from 'next/server'
 // import type { NextRequest } from 'next/server'
-
+ 
 // // This function can be marked `async` if using `await` inside
 // export function middleware(request: NextRequest) {
-//   // Since authentication is being removed, we won't redirect the user.
-//   // Simply allow access to any page.
-//   return NextResponse.next();
+//   const path=request.nextUrl.pathname
+//   const isPublicPath=path==='/login' || path==='/register'
+//   const token=request.cookies.get("token")?.value ||''
+//   if(isPublicPath && token){
+//     return NextResponse.redirect(new URL('/', request.url))
+//   }
+//   if(!isPublicPath && !token){
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
 // }
-
-// // You can still use matcher to define the paths the middleware applies to
+ 
+// // See "Matching Paths" below to learn more
 // export const config = {
 //   matcher: [
 //     '/register',
@@ -49,3 +26,26 @@ export const config = {
 //     '/'
 //   ],
 // }
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  // Since authentication is being removed, we won't redirect the user.
+  // Simply allow access to any page.
+  return NextResponse.next();
+}
+
+// You can still use matcher to define the paths the middleware applies to
+export const config = {
+  matcher: [
+    '/register',
+    '/login',
+    '/blog',
+    '/about',
+    '/contact',
+    '/createBlog',
+    '/'
+  ],
+}
